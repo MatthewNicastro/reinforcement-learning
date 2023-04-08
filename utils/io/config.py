dd6ed298719d54d1055b0a814ee5bec1f58ef640
MatthewNicastro <15mrn1@queensu.ca>
2023-04-07 20:59:11 -0400

    INITIAL COMMIT

---
+++ utils/io/config.py
@@ -0,0 +1,22 @@
+from typing import TypeVar, Type
+from pathlib import Path
+from yaml import load, FullLoader
+T = TypeVar('T')
+
+
+def load_config(config_class: Type[T], path: Path) -> T:
+    """
+    Load a configuration file in YAML format and create an instance of a
+    specified class.
+
+    Args:
+        config_class (type): The class of the configuration object to create.
+        path (pathlib.Path): The path to the YAML configuration file.
+
+    Returns:
+        An instance of the specified configuration class.
+    """
+    with open(path, 'r') as f:
+        yaml_data = load(f, Loader=FullLoader)
+    data = config_class(**yaml_data)
+    return data
---
+++ utils/io/model.py
@@ -0,0 +1,43 @@
+from typing import TypeVar, Type
+from torch import save, load
+from torch.nn import Module
+from pathlib import Path
+T = TypeVar('T')
+
+
+def save_model(model: Module, path: Path):
+    """
+    Save a PyTorch model to a file.
+
+    Args:
+        model (torch.nn.Module): The model to save.
+        path (pathlib.Path): The file path to save the model to.
+    """
+    state_dict = model.state_dict()
+    save(state_dict, str(path))
+
+
+def load_model(
+    model_class: Type[T],
+    state_dict_path: Path = None,
+    eval_mode: bool = True
+) -> T:
+    """
+    Load a PyTorch model from a file.
+
+    Args:
+        model_class (type): The class of the model to create.
+        state_dict_path (pathlib.Path): The file path to the saved model state
+                                        dictionary.
+        eval_mode (bool): Whether to set the model to evaluation mode.
+                          Defaults to True.
+
+    Returns:
+        The loaded model instance.
+    """
+    model: Module = model_class()
+    state_dict = load(state_dict_path)
+    model.load_state_dict(state_dict)
+    if eval_mode:
+        model.eval()
+    return model
