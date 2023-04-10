7ebdc97c4bf271aac4ecc1bcebf43e867eefab88
MatthewNicastro <15mrn1@queensu.ca>
2023-04-10 01:59:05 -0400

    split up generalized_advantage_estimation_per_trajectory and added tests

--- tests/utils/algorithms/test_advantage_estimation.py
+++ tests/utils/algorithms/test_advantage_estimation.py
@@ -1,7 +1,10 @@
 import torch
 import pytest
-from unittest.mock import MagicMock
-from utils.algorithms.advantage_estimation import generalized_advantage_estimation
+from unittest.mock import MagicMock, patch
+from utils.algorithms.advantage_estimation import (
+    generalized_advantage_estimation_per_trajectory,
+    generalized_advantage_estimation,
+)
 
 
 def test_zero_rewards_values():
@@ -10,7 +13,7 @@ def test_zero_rewards_values():
     gamma = 0.99
     lam = 0.95
 
-    advantages, returns = generalized_advantage_estimation(
+    advantages, returns = generalized_advantage_estimation_per_trajectory(
         rewards=rewards, values=values, gamma=gamma, lam=lam
     )
 
@@ -24,7 +27,7 @@ def test_constant_rewards_zero_values():
     gamma = 0.99
     lam = 0.95
 
-    advantages, returns = generalized_advantage_estimation(
+    advantages, returns = generalized_advantage_estimation_per_trajectory(
         rewards=rewards, values=values, gamma=gamma, lam=lam
     )
 
@@ -47,7 +50,7 @@ def test_rewards_values_with_mock():
     gamma = 0.99
     lam = 0.95
 
-    generalized_advantage_estimation(
+    generalized_advantage_estimation_per_trajectory(
         rewards=mock_rewards_tensor, values=mock_values_tensor, gamma=gamma, lam=lam
     )
 
@@ -60,9 +63,96 @@ def test_varying_gamma_lambda(gamma, lam):
     rewards = torch.tensor([1.0, 2.0, 3.0, 4.0, 5.0])
     values = torch.tensor([0.5, 1.5, 2.5, 3.5, 4.5])
 
-    advantages, returns = generalized_advantage_estimation(
+    advantages, returns = generalized_advantage_estimation_per_trajectory(
         rewards=rewards, values=values, gamma=gamma, lam=lam
     )
 
     assert torch.all(advantages > 0)
     assert torch.all(returns > 0)
+
+
+@patch(
+    "utils.algorithms.advantage_estimation.generalized_advantage_estimation_per_trajectory"
+)
+def test_generalized_advantage_estimation_empty_inputs(mock_gae):
+    mock_gae.return_value = (torch.tensor([0.0]), torch.tensor([0.0]))
+
+    advantages, returns = generalized_advantage_estimation(
+        num_trajectories=0,
+        rewards=[],
+        values=[],
+        discount_factor=0.99,
+        gae_lambda=0.95,
+    )
+
+    assert len(advantages) == 0
+    assert len(returns) == 0
+
+
+@patch(
+    "utils.algorithms.advantage_estimation.generalized_advantage_estimation_per_trajectory"
+)
+def test_generalized_advantage_estimation_single_trajectory(mock_gae):
+    rewards = [torch.tensor([1.0, 1.0, 1.0])]
+    values = [torch.tensor([0.5, 0.5, 0.5])]
+    expected_advantages = [torch.tensor([1.0, 1.0, 1.0])]
+    expected_returns = [torch.tensor([3.0, 2.0, 1.0])]
+
+    mock_gae.side_effect = [
+        (expected_advantages[0], expected_returns[0]),
+    ]
+
+    advantages, returns = generalized_advantage_estimation(
+        num_trajectories=1,
+        rewards=rewards,
+        values=values,
+        discount_factor=0.99,
+        gae_lambda=0.95,
+    )
+
+    assert len(advantages) == 1
+    assert len(returns) == 1
+    assert (advantages[0] == expected_advantages[0]).all()
+    assert (returns[0] == expected_returns[0]).all()
+
+
+@patch(
+    "utils.algorithms.advantage_estimation.generalized_advantage_estimation_per_trajectory"
+)
+def test_generalized_advantage_estimation_multiple_trajectories(mock_gae):
+    rewards = [
+        torch.tensor([1.0, 1.0, 1.0]),
+        torch.tensor([2.0, 2.0, 2.0]),
+    ]
+    values = [
+        torch.tensor([0.5, 0.5, 0.5]),
+        torch.tensor([1.0, 1.0, 1.0]),
+    ]
+    expected_advantages = [
+        torch.tensor([1.0, 1.0, 1.0]),
+        torch.tensor([2.0, 2.0, 2.0]),
+    ]
+    expected_returns = [
+        torch.tensor([3.0, 2.0, 1.0]),
+        torch.tensor([6.0, 4.0, 2.0]),
+    ]
+
+    mock_gae.side_effect = [
+        (expected_advantages[0], expected_returns[0]),
+        (expected_advantages[1], expected_returns[1]),
+    ]
+
+    advantages, returns = generalized_advantage_estimation(
+        num_trajectories=2,
+        rewards=rewards,
+        values=values,
+        discount_factor=0.99,
+        gae_lambda=0.95,
+    )
+
+    assert len(advantages) == 2
+    assert len(returns) == 2
+    assert (advantages[0] == expected_advantages[0]).all()
+    assert (returns[0] == expected_returns[0]).all()
+    assert (advantages[1] == expected_advantages[1]).all()
+    assert (returns[1] == expected_returns[1]).all()
--- utils/algorithms/advantage_estimation.py
+++ utils/algorithms/advantage_estimation.py
@@ -2,7 +2,7 @@ from typing import Tuple
 from torch import Tensor, zeros_like
 
 
-def generalized_advantage_estimation(
+def generalized_advantage_estimation_per_trajectory(
     rewards: Tensor, values: Tensor, gamma: float, lam: float
 ) -> Tuple[Tensor, Tensor]:
     """
@@ -36,4 +36,46 @@ def generalized_advantage_estimation(
         advantages[idx] = delta + gamma * lam * next_value
         next_return = reward + gamma * next_return
         returns[idx] = next_return
+    return advantages, returns
+
+
+def generalized_advantage_estimation(
+    num_trajectories: int,
+    rewards: list,
+    values: list,
+    discount_factor: float,
+    gae_lambda: float,
+) -> Tuple[list, list]:
+    """
+    Compute generalized advantage estimation (GAE) for a batch of trajectories.
+
+    Args:
+        num_trajectories (int): Number of trajectories in the batch.
+        rewards (list): List of reward tensors for each trajectory.
+        values (list): List of value tensors for each trajectory.
+        discount_factor (float): Discount factor for future rewards.
+        gae_lambda (float): GAE lambda parameter.
+
+    Returns:
+        tuple: Tuple containing:
+            - advantages (list): List of advantage tensors for each trajectory.
+            - returns (list): List of return tensors for each trajectory.
+    """
+    advantages = [None for _ in range(num_trajectories)]
+    returns = [None for _ in range(num_trajectories)]
+
+    for trajectory_num in range(num_trajectories):
+        curr_rewards = rewards[trajectory_num]
+        curr_values = values[trajectory_num]
+        (
+            trajectory_advantages,
+            trajectory_returns,
+        ) = generalized_advantage_estimation_per_trajectory(
+            rewards=curr_rewards,
+            values=curr_values,
+            gamma=discount_factor,
+            lam=gae_lambda,
+        )
+        advantages[trajectory_num] = trajectory_advantages
+        returns[trajectory_num] = trajectory_returns
     return advantages, returns
