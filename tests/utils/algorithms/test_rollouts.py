d1383488d97b0883ae8e2be62e50bd6fcf16bfec
MatthewNicastro <15mrn1@queensu.ca>
2023-04-11 00:37:44 -0400

    Added rollout module to compute trajectories and tests

---
+++ tests/utils/algorithms/test_rollouts.py
@@ -0,0 +1,121 @@
+import unittest.mock as mock
+import torch
+from gymnasium import Env
+from utils.algorithms.wrappers import ModelWrapper
+from utils.algorithms.rollouts import compute_trajectories
+
+
+def test_compute_trajectories_zero_steps():
+    env_mock = mock.MagicMock(spec=Env)
+    policy_wrapper_mock = mock.MagicMock(spec=ModelWrapper)
+    value_wrapper_mock = mock.MagicMock(spec=ModelWrapper)
+    reward_func_mock = mock.MagicMock()
+
+    (
+        states,
+        actions,
+        log_probs,
+        rewards,
+        values,
+        num_steps_per_trajectory,
+    ) = compute_trajectories(
+        env=env_mock,
+        state_parser=lambda x: x,
+        policy_wrapper=policy_wrapper_mock,
+        value_wrapper=value_wrapper_mock,
+        reward_func=reward_func_mock,
+        num_steps=0,
+    )
+
+    assert len(states) == 0
+    assert len(actions) == 0
+    assert len(log_probs) == 0
+    assert len(rewards) == 0
+    assert len(values) == 0
+    assert len(num_steps_per_trajectory) == 0
+
+
+def test_compute_trajectories_single_step():
+    env_mock = mock.MagicMock(spec=Env)
+    env_mock.reset.return_value = ([0], False)
+    env_mock.step.return_value = ([1], 1, True, False, {})
+
+    policy_wrapper_mock = mock.MagicMock()
+    policy_wrapper_mock.network = mock.MagicMock(
+        return_value=(torch.tensor(0), torch.tensor(0))
+    )
+    policy_wrapper_mock.output_parser.side_effect = lambda x: x
+
+    value_wrapper_mock = mock.MagicMock()
+    value_wrapper_mock.network = mock.MagicMock(return_value=torch.tensor(0))
+    value_wrapper_mock.output_parser.side_effect = lambda x: x
+
+    reward_func_mock = mock.MagicMock()
+    reward_func_mock.side_effect = lambda _, x: x
+
+    (
+        states,
+        actions,
+        log_probs,
+        rewards,
+        values,
+        num_steps_per_trajectory,
+    ) = compute_trajectories(
+        env=env_mock,
+        state_parser=lambda x: x,
+        policy_wrapper=policy_wrapper_mock,
+        value_wrapper=value_wrapper_mock,
+        reward_func=reward_func_mock,
+        num_steps=1,
+    )
+
+    assert len(states) == 0
+    assert len(actions) == 0
+    assert len(log_probs) == 0
+    assert len(rewards) == 0
+    assert len(values) == 0
+    assert len(num_steps_per_trajectory) == 0
+    assert len(num_steps_per_trajectory) == 0
+
+
+def test_compute_trajectories_multiple():
+    env_mock = mock.MagicMock(spec=Env)
+    env_mock.reset.return_value = ([0], False)
+    env_mock.step.return_value = ([1], 1, False, False, {})
+
+    policy_wrapper_mock = mock.MagicMock()
+    policy_wrapper_mock.network = mock.MagicMock(
+        return_value=(torch.tensor(0), torch.tensor(0))
+    )
+    policy_wrapper_mock.output_parser.side_effect = lambda x: x
+
+    value_wrapper_mock = mock.MagicMock()
+    value_wrapper_mock.network = mock.MagicMock(return_value=torch.tensor(0))
+    value_wrapper_mock.output_parser.side_effect = lambda x: x
+
+    reward_func_mock = mock.MagicMock()
+    reward_func_mock.side_effect = lambda _, x: x
+
+    (
+        states,
+        actions,
+        log_probs,
+        rewards,
+        values,
+        num_steps_per_trajectory,
+    ) = compute_trajectories(
+        env=env_mock,
+        state_parser=lambda x: torch.tensor(x),
+        policy_wrapper=policy_wrapper_mock,
+        value_wrapper=value_wrapper_mock,
+        reward_func=reward_func_mock,
+        num_steps=3,
+    )
+
+    assert len(states) == 1
+    assert len(actions) == 1
+    assert len(log_probs) == 1
+    assert len(rewards) == 1
+    assert len(values) == 1
+    assert len(num_steps_per_trajectory) == 1
+    assert num_steps_per_trajectory[0] == 3
---
+++ utils/algorithms/rollouts.py
@@ -0,0 +1,114 @@
+from gymnasium import Env
+from torch import stack, tensor
+from utils.algorithms.wrappers import ModelWrapper
+from typing import Tuple, Callable
+
+
+def compute_trajectories(
+    env: Env,
+    state_parser: Callable,
+    policy_wrapper: ModelWrapper,
+    value_wrapper: ModelWrapper,
+    reward_func: Callable,
+    num_steps: int,
+) -> Tuple[list, list, list, list, list, list]:
+    """
+    Compute trajectories for the given environment, policy, and value networks.
+
+    Args:
+        env (Env): Gym environment.
+        state_parser (Callable): Function to preprocess state inputs.
+        policy_wrapper (ModelWrapper): Wrapper for the policy network.
+        value_wrapper (ModelWrapper): Wrapper for the value network.
+        reward_func (Callable): Function to compute rewards.
+        num_steps (int): Number of steps to compute.
+
+    Returns:
+        tuple: Tuple containing:
+            - states (list): List of state tensors for each trajectory.
+            - actions (list): List of action tensors for each trajectory.
+            - log_probs (list): List of log probability tensors for each trajectory.
+            - rewards (list): List of reward tensors for each trajectory.
+            - values (list): List of value tensors for each trajectory.
+            - num_steps_per_trajectory (list): List of the number of steps for each trajectory.
+    """
+    states = []
+    actions = []
+    log_probs = []
+    rewards = []
+    values = []
+    num_steps_per_trajectory = []
+    step_count = 0
+    state = env.reset()[0]
+    done = False
+    (
+        curr_rewards,
+        curr_states,
+        curr_actions,
+        curr_probs,
+        curr_values,
+    ) = _init_trajectory()
+    # Compute trajectories
+    for _ in range(num_steps):
+        state_t = state_parser(state)
+        action_log_prob = policy_wrapper.network(state=state_t)
+        action, log_prob = policy_wrapper.output_parser(action_log_prob)
+        action, log_prob = action.item(), log_prob.item()
+        new_state, reward, done, truncated, info = env.step(action)
+        value_output = value_wrapper.network(state=state_t)
+        value = value_wrapper.output_parser(value_output)
+        value = value.item()
+        curr_rewards.append(reward_func(curr_rewards, reward))
+        curr_states.append(state_t)
+        curr_actions.append(action)
+        curr_probs.append(log_prob)
+        curr_values.append(value)
+
+        state = new_state
+        if done:
+            if step_count > 0:
+                num_steps_per_trajectory.append(step_count)
+                states.append(stack(curr_states))
+                actions.append(tensor(curr_actions))
+                log_probs.append(tensor(curr_probs))
+                rewards.append(tensor(curr_rewards))
+                values.append(tensor(curr_values))
+            state = env.reset()[0]
+            step_count = 0
+            done = False
+            (
+                curr_rewards,
+                curr_states,
+                curr_actions,
+                curr_probs,
+                curr_values,
+            ) = _init_trajectory()
+        else:
+            step_count += 1
+
+    if step_count > 1:
+        num_steps_per_trajectory.append(step_count)
+        states.append(stack(curr_states))
+        actions.append(tensor(curr_actions))
+        log_probs.append(tensor(curr_probs))
+        rewards.append(tensor(curr_rewards))
+        values.append(tensor(curr_values))
+
+    return (
+        states,
+        actions,
+        log_probs,
+        rewards,
+        values,
+        num_steps_per_trajectory,
+    )
+
+
+def _init_trajectory() -> Tuple[list, list, list, list, list]:
+    """
+    Initialize a trajectory with empty lists for rewards, states, actions, probabilities, and values.
+
+    Returns:
+        tuple: Tuple of empty lists (rewards, states, actions, probabilities, values).
+    """
+    return ([], [], [], [], [])
