b68df8d28904ecb74441632e2d5d3326e0da2771
MatthewNicastro <15mrn1@queensu.ca>
2023-04-11 00:40:59 -0400

    Added PPO algorithm and unit tests

---
+++ tests/utils/algorithms/test_ppo.py
@@ -0,0 +1,110 @@
+import torch
+import pytest
+from unittest.mock import MagicMock
+from utils.algorithms.ppo import proximal_policy_optimization
+
+
+class DummyEnv:
+    def reset(self):
+        return [1.0], 0, False, {}
+
+    def step(self, action):
+        return [1.0], 0, False, False, {}
+
+
+class DummyPolicyModelWrapper:
+    def __init__(self, output):
+        self.output = output
+
+    def network(self, state):
+        return self.output
+
+    def output_parser(self, output, index=None):
+        return (
+            (output.to(int), output)
+            if index is None
+            else (output[index].to(int), output[index])
+        )
+
+    def update(self, loss, retain_graph=None):
+        pass
+
+
+class DummyValueModelWrapper:
+    def __init__(self, output):
+        self.output = output
+
+    def network(self, state):
+        return self.output
+
+    def output_parser(self, output, index=None):
+        return output if index is None else output[index]
+
+    def update(self, loss, retain_graph=None):
+        pass
+
+
+def dummy_state_parser(state):
+    return torch.tensor([state])
+
+
+def dummy_value_loss(preds, target):
+    return torch.mean(torch.square(preds - target))
+
+
+def dummy_reward_func(curr_rewards, reward):
+    return reward
+
+
+@pytest.fixture
+def ppo_args():
+    env = DummyEnv()
+    state_parser = dummy_state_parser
+    policy_wrapper = DummyPolicyModelWrapper(torch.tensor([0.0]))
+    value_wrapper = DummyValueModelWrapper(torch.tensor([0.0]))
+    value_loss = dummy_value_loss
+    reward_func = dummy_reward_func
+    epochs = 1
+    num_steps = 10
+    updates_per_epoch = 1
+
+    return (
+        env,
+        state_parser,
+        policy_wrapper,
+        value_wrapper,
+        value_loss,
+        reward_func,
+        epochs,
+        num_steps,
+        updates_per_epoch,
+    )
+
+
+def test_proximal_policy_optimization(ppo_args):
+    logger = proximal_policy_optimization(*ppo_args)
+
+    assert "policy_losses" in logger
+    assert "value_losses" in logger
+    assert "num_steps" in logger
+    assert len(logger["policy_losses"]) == 1
+    assert len(logger["value_losses"]) == 1
+    assert len(logger["num_steps"]) == 1
+
+
+def test_mocked_policy_update(ppo_args):
+    policy_wrapper = ppo_args[2]
+    policy_wrapper.update = MagicMock()
+
+    proximal_policy_optimization(*ppo_args)
+
+    policy_wrapper.update.assert_called_once()
+
+
+def test_mocked_value_update(ppo_args):
+    value_wrapper = ppo_args[3]
+    value_wrapper.update = MagicMock()
+
+    proximal_policy_optimization(*ppo_args)
+
+    value_wrapper.update.assert_called_once()
---
+++ utils/algorithms/ppo.py
@@ -0,0 +1,133 @@
+from tqdm import tqdm
+from gymnasium import Env
+from typing import Callable
+from utils.algorithms.wrappers import ModelWrapper
+from utils.algorithms.rollouts import compute_trajectories
+from utils.algorithms.policy_loss import clipped_surrogate_objective
+from utils.algorithms.advantage_estimation import generalized_advantage_estimation
+
+
+def proximal_policy_optimization(
+    env: Env,
+    state_parser: Callable,
+    policy_wrapper: ModelWrapper,
+    value_wrapper: ModelWrapper,
+    value_loss: Callable,
+    reward_func: Callable,
+    epochs: int,
+    num_steps: int,
+    updates_per_epoch: int,
+    discount_factor: float = 0.99,
+    gae_lambda: float = 0.95,
+    clipping_parameter: float = 0.2,
+) -> dict:
+    """
+    Train an agent using Proximal Policy Optimization (PPO) algorithm.
+
+    Args:
+        env (Env): Gym environment.
+        state_parser (Callable): Function to preprocess state inputs.
+        policy_wrapper (ModelWrapper): Wrapper for the policy network.
+        value_wrapper (ModelWrapper): Wrapper for the value network.
+        value_loss (Callable): Function to compute value loss.
+        reward_func (Callable): Function to compute rewards.
+        epochs (int): Number of training epochs.
+        num_steps (int): Number of steps per epoch.
+        updates_per_epoch (int): Number of optimization steps per epoch.
+        discount_factor (float): Discount factor for future rewards. Defaults to 0.99.
+        gae_lambda (float): GAE lambda parameter. Defaults to 0.95.
+        clipping_parameter (float): Clipping parameter for PPO. Defaults to 0.2.
+
+    Returns:
+        logger (dict): A dictionary with the following structure
+                       "policy_losses" (list): all the policy losses (epochs * updates_per_epoch),
+                       "value_losses" (list): all the value losses (epochs * updates_per_epoch),,
+                       "num_steps" (list): number of steps per trajectory (epochs * num_steps),
+    """
+    # Initialize logger
+    logger = _init_logger()
+
+    # Main training loop
+    for _ in tqdm(range(epochs)):
+        (
+            states,
+            actions,
+            log_probs,
+            rewards,
+            values,
+            num_steps_per_trajectory,
+        ) = compute_trajectories(
+            env,
+            state_parser,
+            policy_wrapper,
+            value_wrapper,
+            reward_func,
+            num_steps,
+        )
+        logger["num_steps"] += num_steps_per_trajectory
+
+        num_trajectories = len(states)
+        if num_trajectories == 0:
+            raise "No trajectories"
+        advantages, returns = generalized_advantage_estimation(
+            num_trajectories, rewards, values, discount_factor, gae_lambda
+        )
+        # Update policy and value functions
+        for i in range(updates_per_epoch):
+            policy_func_loss = 0.0
+            value_func_loss = 0.0
+            for trajectory_num in range(num_trajectories):
+                trajectory_states_t = states[trajectory_num]
+                trajectory_actions = actions[trajectory_num]
+                trajectory_log_probs = log_probs[trajectory_num]
+                trajectory_advantages = advantages[trajectory_num]
+                trajectory_advantages = (
+                    trajectory_advantages - trajectory_advantages.mean()
+                ) / (trajectory_advantages.std() + 1e-8)
+                trajectory_new_action_log_probs = policy_wrapper.network(
+                    trajectory_states_t
+                )
+                _, trajectory_new_log_probs = policy_wrapper.output_parser(
+                    trajectory_new_action_log_probs, index=trajectory_actions
+                )
+                policy_func_loss -= clipped_surrogate_objective(
+                    log_current_probs=trajectory_new_log_probs,
+                    log_old_probs=trajectory_log_probs.detach(),
+                    advantages=trajectory_advantages,
+                    epsilon=clipping_parameter,
+                )
+
+                trajectory_returns = returns[trajectory_num]
+                trajectory_new_value_preds = value_wrapper.network(
+                    trajectory_states_t
+                ).squeeze()
+
+                value_func_loss += value_loss(
+                    trajectory_new_value_preds, trajectory_returns
+                )
+
+            policy_func_loss /= num_trajectories
+            value_func_loss /= num_trajectories
+            policy_wrapper.update(
+                loss=policy_func_loss,
+                retain_graph=(i != updates_per_epoch - 1),
+            )
+            logger["policy_losses"].append(policy_func_loss.item())
+
+            value_wrapper.update(loss=value_func_loss)
+            logger["value_losses"].append(value_func_loss.item())
+    return logger
+
+
+def _init_logger():
+    """
+    Initialize a logger to store training statistics.
+
+    Returns:
+        dict: Dictionary containing keys and empty lists for policy_losses, value_losses, and num_steps.
+    """
+    return {
+        "policy_losses": [],
+        "value_losses": [],
+        "num_steps": [],
+    }
